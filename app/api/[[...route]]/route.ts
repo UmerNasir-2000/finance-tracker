f18475bc700814a7d6590e84ae8ac22ab639cd93
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-25 22:01:18 +0500

    feat: add zod validator for hono

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,10 +1,16 @@
 import { clerkMiddleware } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { z } from "zod";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+
+const schema = z.object({
+  name: z.string().min(5),
+});
 
 app.use("*", clerkMiddleware());
 app.notFound((c) => c.json({ message: "Not Found" }, 404));
@@ -12,6 +18,14 @@ app.notFound((c) => c.json({ message: "Not Found" }, 404));
 app.get("/hello", (c) => {
   return c.json({
     message: "Hello Next.js!",
+  });
+});
+
+app.get("/hello/:name", zValidator("param", schema), (c) => {
+  const name = c.req.param("name");
+  return c.json({
+    message: "Hello Next.js!",
+    name,
   });
 });
 

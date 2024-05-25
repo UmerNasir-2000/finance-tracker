2ca3aa0790a4701b8d908c17c64b8cf626f0c31e
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-25 17:21:49 +0500

    refactor: extract duplicate code in layout for auth pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,42 +1,5 @@
-import Image from "next/image";
-import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
-import { Skeleton } from "@/components/ui/skeleton";
+import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
-  return (
-    <>
-      <ClerkLoading>
-        <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-          <div className="flex flex-col justify-center items-center">
-            <div className="flex flex-col space-y-3">
-              <Skeleton className="h-[400px] w-[400px] rounded-xl" />
-              <div className="space-y-2">
-                <Skeleton className="h-4 w-[400px]" />
-                <Skeleton className="h-4 w-[400px]" />
-              </div>
-            </div>
-          </div>
-          <div className="hidden bg-blue-600 text-white lg:block">
-            <div className="h-full flex flex-col justify-center items-center gap-y-4">
-              <Image src="/logo.svg" alt="Logo" width={150} height={150} />
-              <p className="text-sm">One place to manage it all.</p>
-            </div>
-          </div>
-        </section>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-          <div className="flex flex-col justify-center items-center">
-            <SignUp />
-          </div>
-          <div className="hidden bg-blue-600 text-white lg:block">
-            <div className="h-full flex flex-col justify-center items-center gap-y-4">
-              <Image src="/logo.svg" alt="Logo" width={150} height={150} />
-              <p className="text-sm">One place to manage it all.</p>
-            </div>
-          </div>
-        </section>
-      </ClerkLoaded>
-    </>
-  );
+export default function SignUpPage() {
+  return <SignUp />;
 }

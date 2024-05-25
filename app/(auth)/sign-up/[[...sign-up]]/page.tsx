31d28af6c8f57f7bdad6e7332ff00a376e4984a8
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-25 13:47:10 +0500

    feat: add loading skeletons for auth pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,28 @@
-import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
-import { LoaderCircle } from "lucide-react";
 import Image from "next/image";
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Page() {
   return (
     <>
       <ClerkLoading>
-        <LoaderCircle className="animate-spin" />
+        <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[400px] w-[400px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[400px]" />
+                <Skeleton className="h-4 w-[400px]" />
+              </div>
+            </div>
+          </div>
+          <div className="hidden bg-blue-600 text-white lg:block">
+            <div className="h-full flex flex-col justify-center items-center gap-y-4">
+              <Image src="/logo.svg" alt="Logo" width={150} height={150} />
+              <p className="text-sm">One place to manage it all.</p>
+            </div>
+          </div>
+        </section>
       </ClerkLoading>
       <ClerkLoaded>
         <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
@@ -15,7 +31,7 @@ export default function Page() {
           </div>
           <div className="hidden bg-blue-600 text-white lg:block">
             <div className="h-full flex flex-col justify-center items-center gap-y-4">
-              <Image src="/logo.svg" alt="Logo" width={200} height={200} />
+              <Image src="/logo.svg" alt="Logo" width={150} height={150} />
               <p className="text-sm">One place to manage it all.</p>
             </div>
           </div>

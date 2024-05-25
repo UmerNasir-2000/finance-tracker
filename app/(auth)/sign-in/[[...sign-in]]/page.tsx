8b42246aa1b85f325efc13d66ad66952f9629d1b
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-25 13:27:53 +0500

    refactor: improve ui for auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import { LoaderCircle } from "lucide-react";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -8,9 +9,17 @@ export default function Page() {
         <LoaderCircle className="animate-spin" />
       </ClerkLoading>
       <ClerkLoaded>
-        <div className="min-h-screen flex justify-center items-center">
-          <SignIn />
-        </div>
+        <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col justify-center items-center">
+            <SignIn />
+          </div>
+          <div className="hidden bg-blue-600 text-white lg:block">
+            <div className="h-full flex flex-col justify-center items-center gap-y-4">
+              <Image src="/logo.svg" alt="Logo" width={200} height={200} />
+              <p className="text-sm">One place to manage it all.</p>
+            </div>
+          </div>
+        </section>
       </ClerkLoaded>
     </>
   );

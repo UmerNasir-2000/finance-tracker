5d1a0c66dd7a6ec3fa340cc47d29d02c143118c6
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-25 23:07:02 +0500

    feat: add mobile menu

--- app/(dashboard)/components/header/index.tsx
+++ app/(dashboard)/components/header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-gradient-to-b from-blue-500 to-blue-700 text-white px-4 py-8 lg:px-14 pb-36">
       <div className="max-w-screen-2xl mx-auto">
-        <div className="w-full flex justify-center items-center mb-14">
+        <div className="w-full flex justify-start items-center mb-14">
           <div className="flex items-center lg:gap-x-16">
             <Logo />
             <Navigation />
--- app/(dashboard)/components/navigation/index.tsx
+++ app/(dashboard)/components/navigation/index.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { useMedia } from "react-use";
 
 type NavLink = {
   href: string;
@@ -35,6 +39,46 @@ const links: NavLink[] = [
 
 const Navigation = () => {
   const path = usePathname();
+  const router = useRouter();
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onNavItemClick = useCallback((href: string) => {
+    router.push(href);
+    setIsMobileMenuOpen(false);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {links.map((link) => (
+              <Button
+                key={link.href}
+                variant={link.href === path ? "secondary" : "ghost"}
+                className="w-full justify-start"
+                onClick={() => onNavItemClick(link.href)}
+              >
+                {link.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
 
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">

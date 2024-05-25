de37d8ae2dc0b57bd627c503a7b2fa276050b142
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-25 22:49:46 +0500

    feat: style nav items

--- app/(dashboard)/components/navigation/index.tsx
+++ app/(dashboard)/components/navigation/index.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type NavLink = {
   href: string;
@@ -29,10 +34,23 @@ const links: NavLink[] = [
 ] as const;
 
 const Navigation = () => {
+  const path = usePathname();
+
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
       {links.map((link) => (
-        <Link href={link.href}>{link.label}</Link>
+        <Button
+          key={link.href}
+          asChild
+          variant="outline"
+          size="sm"
+          className={cn(
+            "w-full lg:w-auto justify-between font-normal hover:bg-white/20  hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+            path === link.href ? "bg-white/10 text-white" : "bg-transparent"
+          )}
+        >
+          <Link href={link.href}>{link.label}</Link>
+        </Button>
       ))}
     </nav>
   );

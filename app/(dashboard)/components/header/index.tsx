78ca268f72458c365f70600644dcf8f0b013b28a
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-26 12:38:21 +0500

    feat: add drizzle orm

--- app/(dashboard)/components/header/index.tsx
+++ app/(dashboard)/components/header/index.tsx
@@ -1,3 +1,4 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import Logo from "../logo";
 import Navigation from "../navigation";
 
@@ -5,10 +6,16 @@ const Header = () => {
   return (
     <header className="bg-gradient-to-b from-blue-500 to-blue-700 text-white px-4 py-8 lg:px-14 pb-36">
       <div className="max-w-screen-2xl mx-auto">
-        <div className="w-full flex justify-start items-center mb-14">
+        <div className="w-full flex justify-center items-center mb-14">
           <div className="flex items-center lg:gap-x-16">
             <Logo />
             <Navigation />
+            <div className="ml-auto">
+              <ClerkLoaded>
+                <UserButton afterSignOutUrl="/" />
+              </ClerkLoaded>
+              <ClerkLoading></ClerkLoading>
+            </div>
           </div>
         </div>
       </div>
